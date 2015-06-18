854390db6359a3ba944ee2f57e39b21414c9ed2a
Richard Wong <rk2wong@uwaterloo.ca>
2015-06-18 19:49:29 -0400

    wtf mate

--- ComputerPlayer.cpp
+++ ComputerPlayer.cpp
@@ -1,10 +1,6 @@
 #include "ComputerPlayer.h"
 
-<<<<<<< HEAD
 ComputerPlayer::ComputerPlayer() {}
-=======
-ComputerPlayer::ComputerPlayer(): Player(){}
->>>>>>> 5f260bad65311f6afbf2abc73bfc85394ed222b9
 
 void ComputerPlayer::playTurn(){
 
