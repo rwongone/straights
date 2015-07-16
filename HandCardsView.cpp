feab7a6d3bf366adf7404810203a347d9dcbecc8
Richard Wong <rk2wong@uwaterloo.ca>
2015-07-15 22:33:16 -0400

    O BOBYBBYYBB

--- HandCardsView.cpp
+++ HandCardsView.cpp
@@ -15,9 +15,8 @@ HandCardsView::HandCardsView(Gtk::Window &parent, Game* game, GameController* co
     images_[i].set("img/nothing.png");
     discardIcons_[i].set("img/delete.png");
     clickableImages_[i].set_image(images_[i]);
-    discardButtons_[i].set_image(discardIcons_[i]);
     cards_.attach(clickableImages_[i], i, i + 1, 0, 1);
-    cards_.attach(discardButtons_[i], i, i + 1, 1, 2);
+    cards_.attach(discardIcons_[i], i, i + 1, 1, 2);
   }
 
   cards_.set_col_spacings(2);
@@ -45,13 +44,12 @@ void HandCardsView::update() {
   currentPlayer_ = game_->getPlayer(currentPlayerIndex);
 
   std::vector<Card*> hand = currentPlayer_->getHand();
-  setHand(hand);
+  setHand(currentPlayerIndex, hand);
   std::vector<Card*> legalMoves = game_->getLegalMoves(currentPlayerIndex);
 
   if (legalMoves.size() > 0) {
     for (int i=0; i<hand.size(); i++) {
       discardIcons_[i].clear();
-      discardButtonsHandlers_[i].disconnect();
       for (auto it=legalMoves.begin(); it!=legalMoves.end(); ++it) {
         if (*hand[i] == **it) {
           discardIcons_[i].set("img/up.png");
@@ -62,20 +60,22 @@ void HandCardsView::update() {
   }
 }
 
-void HandCardsView::setHand(std::vector<Card*> hand) {
+void HandCardsView::setHand(const int index, std::vector<Card*> hand) {
+  bool canPlay = game_->getLegalMoves(index).size() > 0;
+
   for (int i=0; i<13; i++) {
     clickableImagesHandlers_[i].disconnect();
-    discardButtonsHandlers_[i].disconnect();
   }
 
   for (unsigned int i=0; i<hand.size(); i++) {
-    int currentPlayerIndex = game_->getCurrentPlayer();
     Card theCard = *hand[i];
-
     images_[i].set(toImageFile(theCard));
     discardIcons_[i].set("img/delete.png");
-    clickableImagesHandlers_[i] = clickableImages_[i].signal_clicked().connect(sigc::bind<int, Card>(sigc::mem_fun(*this, &HandCardsView::cardInHandClicked), currentPlayerIndex, theCard));
-    discardButtonsHandlers_[i] = discardButtons_[i].signal_clicked().connect(sigc::bind<int, Card>(sigc::mem_fun(*this, &HandCardsView::discardButtonClicked), currentPlayerIndex, theCard));
+    if (canPlay) {
+      clickableImagesHandlers_[i] = clickableImages_[i].signal_clicked().connect(sigc::bind<int, Card>(sigc::mem_fun(*this, &HandCardsView::cardInHandClicked), index, theCard));
+    } else {
+      clickableImagesHandlers_[i] = clickableImages_[i].signal_clicked().connect(sigc::bind<int, Card>(sigc::mem_fun(*this, &HandCardsView::discardButtonClicked), index, theCard));
+    }
   }
   for (int i=hand.size(); i<13; i++) {
     images_[i].set("img/nothing.png");
