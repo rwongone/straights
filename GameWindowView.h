851929cc4b588f5378286c053a9169cd68ea2ed0
Nosajool <jmloo@uwaterloo.ca>
2015-08-07 12:37:02 -0400

    THIS SHOULD NOT BREAK ANYTHING

--- GameWindowView.h
+++ GameWindowView.h
@@ -20,13 +20,13 @@ public:                                                      // PUBLIC
   virtual void update();                                       // Update view when notify() is called
   void assignPlayerType(std::string, int);                     // Tell controller to assign human or computer player
 private:                                                     // PRIVATE
+  Game* game_;                                                 // Pointer to the model
+  GameController* controller_;                                 // Pointer to the controller
   Gtk::VBox mainVBox;                                          // VBox containing the 4 sections of the view
   TopMenuView topMenu;                                         // View containing "Start game with seed" and "Quit game" buttons
   CardTableView cardTable;                                     // View containing the cards that have been played on the table
   PlayerListView playerList;                                   // View containing the player points, discards and type toggle
   HandCardsView handCards;                                     // View containing the current player's cards
-  Game* game_;                                                 // Pointer to the model
-  GameController* controller_;                                 // Pointer to the controller
 };
 
 #endif
